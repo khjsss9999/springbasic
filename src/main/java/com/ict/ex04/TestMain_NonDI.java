8a482ba67a3a7f39a9717a4cdc72f7eacf9fcfa0
khjsss999 <khjsss999@naver.com>
2024-03-18 17:36:20 +0900

    2024_03_18

---
+++ src/main/java/com/ict/ex04/TestMain_NonDI.java
@@ -0,0 +1,14 @@
+package com.ict.ex04;
+
+public class TestMain_NonDI {
+	public static void main(String[] args) {
+		Age age = new Age();
+		MyProcess mp = new MyProcess("홍길동","940808","1",age);
+		
+		
+		
+	}
+	
+
+	
+}
